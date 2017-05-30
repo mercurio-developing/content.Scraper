6db4730bc4f39cd9d9fcc01e158fc1d5ff5a3e53
mercuriodevelop <mercuriodevelop@gmail.com>
2017-05-29 23:34:53 -0600

    ok

--- scraper.js
+++ scraper.js
@@ -12,8 +12,7 @@ var date = new Date().toISOString().//date to string using ISO standard
             replace(/T/, ' ').      // replace T with a space
             replace(/\..+/, '');   // delete the dot and everything after
             date = date.substr(0,10); //and use only 10 character and delete the rest
-
-
+const fields = ['Title', 'Price', 'imgURL','URL','Time'];  //fields of CSV
 var dataFolder = "./data"
 
   x('http://shirts4mike.com/shirts.php', 'ul.products li', //I define the url and scope
@@ -22,25 +21,33 @@ var dataFolder = "./data"
     Price: x('a@href', '.price'),
     imgURL:x('a@href', 'img@src'),
     URL:('a@href','a@href')
-  }])(function(err, obj){//callback function
+  }])(function(err, shirts){//callback function
     //I define the variable of error message not found
-    let errNotFound = "“There’s been a 404 error. Cannot connect to the to http://shirts4mike.com.”";
 
     if (err) {//if it doesnt connect to server
+
+        let errNotFound = "“There’s been a 404 error. Cannot connect to the to http://shirts4mike.com.”";
+
         console.log(errNotFound);
         let wstream = fs.createWriteStream('./scraper-error.log'); //create the log file
         wstream.write(`${errNotFound} [${gmt}]<error message>`);//write the message in log file
-       }
+       } else {
 
       if (!fs.existsSync(dataFolder)) {// If ./data doesn't exist, create it
             fs.mkdirSync(dataFolder);
           } 
-     
-         const fields = ['Title', 'Price', 'imgURL','URL'];  //fields of CSV
-        let csv = json2csv({ data: obj, fields: fields}); //json to csv
+          
+        var Time = new Date().toTimeString().split(" ")[0]; //i capture the time
+
+        for (var i = 0; i < shirts.length; i+= 1){ //i set the time in the array with for
+          shirts[i].Time = Time;
+        }
+
+        let csv = json2csv({ data: shirts, fields: fields}); //json to csv
 
         fs.writeFile('./data/'+ date +'.csv', csv, function(err) { //csv is written in data folder.
         if (err) throw err;
           console.log('file saved');
         });
+        }
       });
